b91d5a61aef3d70f805021849683f72be761c4b1
raxakk <9415813+raxakk@users.noreply.github.com>
2026-05-14 13:57:01 +0200

    Initial commit

---
+++ Swiftolution/SwiftolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftolutionApp.swift
+//  Swiftolution
+//
+//  Created by raxakk on 14.05.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftolutionTests/SwiftolutionTests.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftolutionTests.swift
+//  SwiftolutionTests
+//
+//  Created by raxakk on 14.05.26.
+//
+
+import Testing
+@testable import Swiftolution
+
+struct SwiftolutionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
